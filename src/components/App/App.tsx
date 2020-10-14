26d6379d121e3328a49f8d64fde6dd713a3e9c99
Ivan Petrukhin <uWynell@yandex.ru>
2020-10-14 20:13:01 +0300

    Separate SubtitleDisplay component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Header, Input, Table } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
+import SubtitleDisplay from '../SubtitleDisplay/SubtitleDisplay';
 import SubtitleLoad from '../SubtitleLoad/SubtitleLoad';
 
 import './App.css';
@@ -10,49 +11,8 @@ function App() {
       <Header size="huge">SubSearch</Header>
 
       <SubtitleLoad />
-
-      <Input
-        label="Subtitle search query"
-        type="text"
-        fluid
-        
-      />
-
-      <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Timecode</Table.HeaderCell>
-            <Table.HeaderCell>Text</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <a href="https://yandex.ru">00:00:00</a>
-            </Table.Cell>
-            <Table.Cell>
-              Hello, <b>world</b>!
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>
-              <a href="https://yandex.ru">00:06:66</a>
-            </Table.Cell>
-            <Table.Cell>
-              This <b>world</b> - is the most liked <b>world</b> by me
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>
-              <a href="https://yandex.ru">00:13:37</a>
-            </Table.Cell>
-            <Table.Cell>
-              I just created a new <b>world</b> in Minecraft
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+      <SubtitleDisplay />
+      
     </Container>
   );
 }
--- src/components/SubtitleDisplay/SubtitleDisplay.tsx
+++ src/components/SubtitleDisplay/SubtitleDisplay.tsx
@@ -1,1 +1,48 @@
-export default {}
+import React, { PropsWithChildren } from 'react';
+import { Input, Table } from 'semantic-ui-react';
+
+const mockSubtitleDate = {
+  '00:00': 'Hello, world',
+  '01:12': 'Bye, world',
+};
+
+function Emphasize(props: PropsWithChildren<{ children: string, text: string }>) {
+  const parts: Array<any> = props.children.split(props.text);
+  const len = parts.length;
+  for (let i = 1; i < len; i++) {
+    parts.splice(2 * i - 1, 0, <b>{props.text}</b>)
+  }
+  return <>{parts}</>;
+}
+
+function SubtitleDisplay() {
+  return (
+    <React.Fragment>
+      <Input label="Subtitle search query" type="text" fluid />
+
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Timecode</Table.HeaderCell>
+            <Table.HeaderCell>Text</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {Object.entries(mockSubtitleDate).map(([timecode, text]) => (
+            <Table.Row>
+              <Table.Cell>
+                <a href="https://yandex.ru">{timecode}</a>
+              </Table.Cell>
+              <Table.Cell>
+                <Emphasize text="world">{text}</Emphasize>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </React.Fragment>
+  );
+}
+
+export default SubtitleDisplay;
