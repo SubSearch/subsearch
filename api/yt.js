5b4726722888129180b7362d49c76ac93a2a253e
Wynell <65278392+uWynell@users.noreply.github.com>
2021-07-13 17:36:12 +0300

    Update yt.js

--- api/yt.js
+++ api/yt.js
@@ -3,6 +3,6 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   const { id } = req.query
   if (!id) return res.status(200);
-  const response = await fetch(`https://youtube.com/get_video_info?video_id=${id}`);
+  const response = await fetch(`https://www.youtube.com/get_video_info?video_id=${id}&eurl=https%3A%2F%2Fyoutube.googleapis.com%2Fv%2F${id}&html5=1&c=TVHTML5&cver=6.20180913`);
   res.status(200).send(await response.text());
 }
